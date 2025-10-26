2ab20f4a22bc1872946732e852b989c8ae1ab93f
shivam singh <shivamkumarsingh10000@gmail.com>
2025-10-26 17:16:13 +0530

    Add size argument to execute_report method

--- generate_report.py
+++ generate_report.py
@@ -1,3 +1,4 @@
+import traceback
 import argparse
 import os
 from factory.transaction_fetcher_factory import TransactionFetcherFactory
@@ -19,20 +20,26 @@ class GenerateReport:
             required=True,
             help='Input ethereum address'
         )
+        parser.add_argument(
+            '-s', '--size',
+            required=True,
+            help='Input ethereum txn count'
+        )
         return parser
 
-    def execute_report(self, eth_address):
+    def execute_report(self, eth_address, large_txn_count=False):
         print(f"Generating report...")
         api_key = os.environ.get('ETHER_SCAN_API_KEY')
         base_url = os.environ.get('ETHER_SCAN_BASE_URL')
-        TransactionFetcherFactory.fetch_transactions('ether', eth_address, api_key=api_key, base_url=base_url)
+        large_txn = large_txn_count == 'true'
+        TransactionFetcherFactory.fetch_transactions('ether', large_txn, eth_address, api_key=api_key, base_url=base_url)
 
     def run(self, args=None):
         try:
             parsed_args = self.parser.parse_args(args)
-            self.execute_report(parsed_args.address)
+            self.execute_report(parsed_args.address, parsed_args.size)
         except Exception as e:
-            print(f"Error occured: {e}")
+            traceback.print_exc()
 
 def main():
     load_env()
