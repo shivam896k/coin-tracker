f5e37f60643b4dc325826a018d616bc9598d1086
shivam singh <shivamkumarsingh10000@gmail.com>
2025-10-26 17:16:42 +0530

    Update fetch_transactions to include large_txn_count

--- factory/transaction_fetcher_factory.py
+++ factory/transaction_fetcher_factory.py
@@ -3,8 +3,8 @@ from service.ethereum_transactions import EthereumTransactions
 
 class TransactionFetcherFactory(ABC):
     @staticmethod
-    def fetch_transactions(source, address, api_key=None, base_url=None):
+    def fetch_transactions(source, large_txn_count, address, api_key=None, base_url=None):
         if source == 'ether':
-            EthereumTransactions(api_key=api_key, base_url=base_url).get_ethereum_transactions(address=address)
+            EthereumTransactions(large_txn_count, api_key=api_key, base_url=base_url).get_ethereum_transactions(address=address)
         else:
             raise ValueError('Invalid source')
